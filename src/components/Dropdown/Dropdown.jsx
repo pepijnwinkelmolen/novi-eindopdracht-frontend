1fec3a3ec2d90068253767c5b27de5e0ee325ed7
pepijnwinkelmolen <pepijnwm1@hotmail.com>
2025-04-07 02:42:03 +0200

    feat: finished the category request and logic on the homepage

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,7 +1,11 @@
 import "./Dropdown.css"
 
 function Dropdown(props) {
-    if (props.variant === "categories") {
+    if (props.list === undefined) {
+        return (
+            <p>loading...</p>
+        )
+    } else if (props.variant === "categories") {
         return (
             <div className="main-dropdown">
                 <button className="filter-category-button" type="button">
@@ -11,15 +15,14 @@ function Dropdown(props) {
                     </div>
                 </button>
                 <ul className="main-dropdown-menu">
-                {Object.entries(props.list).map(function([k, v], i)
-                    {
-                        return (
-                            <li key={i} className="dropdown-menu-item">
-                                <Dropdown variant="child" text={k} list={v}/>
-                            </li>
-                        )
-                    })
-                }
+                {props.list.map((k, i) => {
+                    const keys = Object.keys(k);
+                    return (
+                        <li key={i} className="dropdown-menu-item">
+                            <Dropdown variant="child" text={keys[0]} list={Object.entries(k)}/>
+                        </li>
+                    )
+                })}
                 </ul>
             </div>
         )
@@ -29,12 +32,14 @@ function Dropdown(props) {
                 <button className="dropdown-menu-item-button" type="button">{props.text}</button>
                 <ul className="sub-dropdown-menu">
                     {
-                        props.list.map((v, i) => {
-                            return (
-                                <li key={i} className="sub-dropdown-menu-item">
-                                    <button className="dropdown-menu-item-button">{v}</button>
-                                </li>
-                            )
+                        props.list.map((v) => {
+                            return (v[1].map((n, i) => {
+                                return (
+                                    <li key={i} className="sub-dropdown-menu-item">
+                                        <button className="dropdown-menu-item-button">{n}</button>
+                                    </li>
+                                )
+                            }))
                         })
                     }
                 </ul>
